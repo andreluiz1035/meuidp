2d965ebee4bf7332f9b7b0ae9e67b39d002f3aae
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-03 19:23:44 -0300

    publica no aci

---
+++ step3/variables.tf
@@ -0,0 +1,15 @@
+variable "aci_name" {
+  default = "tiririca-aci"
+}
+
+variable "aci_resource_group" {
+  default = "rg-aci"
+}
+
+variable "container_image" {
+  default = "meuacr123xyz.azurecr.io/tiririca:latest"
+}
+
+variable "location" {
+  default = "brazilsouth"
+}
