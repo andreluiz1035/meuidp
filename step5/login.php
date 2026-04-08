5fddad17c1c8df9c86753927e948e909f1389162
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-08 18:24:13 -0300

    5-18:23

--- step5/login.php
+++ step5/login.php
@@ -1,29 +1,65 @@
+```php
 <?php
 session_start();
 require 'utils.php';
 
+// Se já estiver logado, redireciona
+if (isset($_SESSION['user'])) {
+    header("Location: /");
+    exit;
+}
+
+$erro = "";
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $users = getUsers();
 
-    foreach ($users['users'] as $user) {
-        if ($user['username'] === $_POST['username'] &&
-            password_verify($_POST['password'], $user['password'])) {
+    // Sanitiza input
+    $username = trim($_POST['username'] ?? '');
+    $password = trim($_POST['password'] ?? '');
 
+    foreach ($users['users'] as $user) {
+        if (
+            $user['username'] === $username &&
+            (
+                $password === $user['password'] || // plaintext
+                password_verify($password, $user['password']) // bcrypt
+            )
+        ) {
             $_SESSION['user'] = $user;
 
-            // volta para authorize
+            // Redireciona (ex: OAuth authorize)
             $redirect = $_GET['redirect'] ?? '/';
             header("Location: $redirect");
             exit;
         }
     }
 
-    echo "Login inválido";
+    $erro = "Login inválido";
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<?php if ($erro): ?>
+    <p style="color:red;"><?php echo $erro; ?></p>
+<?php endif; ?>
+
 <form method="POST">
-  <input name="username" placeholder="user">
-  <input name="password" type="password">
-  <button>Login</button>
+    <input name="username" placeholder="Usuário" required>
+    <br><br>
+    <input name="password" type="password" placeholder="Senha" required>
+    <br><br>
+    <button type="submit">Entrar</button>
 </form>
+
+</body>
+</html>
+
