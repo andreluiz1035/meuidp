9797ec9b39a6c18bb039608c0de4b0b6ae69993a
andreluiz1035 <andreluiz.mcse@gmail.com>
2026-04-14 14:40:00 -0300

    sandro3

--- step3/aci.tf
+++ step3/aci.tf
@@ -2,37 +2,25 @@ provider "azurerm" {
   features {}
 }
 
-
-#  Managed Identity
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-sleep 1800
-
-# Permissão para o ACI puxar imagem do ACR
-resource "azurerm_role_assignment" "acr_pull" {
-  principal_id         = azurerm_container_group.app.identity[0].principal_id
-  role_definition_name = "AcrPull"
-  scope                = data.azurerm_container_registry.acr.id
-}
-
-1800
-
-# Resource Group do ACI
+# -----------------------------
+# Resource Group
+# -----------------------------
 resource "azurerm_resource_group" "aci" {
   name     = var.aci_resource_group
   location = var.location
 }
 
-# Referência ao ACR existente
+# -----------------------------
+# ACR existente
+# -----------------------------
 data "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = var.acr_resource_group
 }
 
+# -----------------------------
 # Container Group (ACI)
+# -----------------------------
 resource "azurerm_container_group" "app" {
   name                = var.aci_name
   location            = azurerm_resource_group.aci.location
@@ -42,15 +30,17 @@ resource "azurerm_container_group" "app" {
   ip_address_type = "Public"
   dns_name_label  = var.aci_name
 
-  #  Reinício automático (resolve timing do ACR)
   restart_policy = "Always"
 
-  
+  # Managed Identity (ESSENCIAL)
+  identity {
+    type = "SystemAssigned"
+  }
 
   container {
-    name   = "app"
+    name  = "app"
 
-    image  = "${data.azurerm_container_registry.acr.login_server}/${var.container_image}"
+    image = "${data.azurerm_container_registry.acr.login_server}/${var.container_image}"
 
     cpu    = "0.5"
     memory = "1.0"
@@ -62,3 +52,16 @@ resource "azurerm_container_group" "app" {
   }
 }
 
+# -----------------------------
+# Permissão do ACI para puxar imagem do ACR
+# -----------------------------
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+
+  principal_id = azurerm_container_group.app.identity[0].principal_id
+
+  depends_on = [
+    azurerm_container_group.app
+  ]
+}
